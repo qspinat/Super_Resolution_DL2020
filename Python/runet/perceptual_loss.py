2f1c7b9fa946f3c04ecea7075ac6f77bd67651a8
qspinat <qspinat@gmail.com>
2021-01-20 21:19:31 +0100

    SSIM and PSNR

--- Python/runet/perceptual_loss.py
+++ Python/runet/perceptual_loss.py
@@ -20,3 +20,20 @@ def perceptual_loss(Img1,Img2,device=torch.device("cuda:0" if torch.cuda.is_avai
     loss = nn.MSELoss()(features1,features2)
     return loss
     
+def PSNR(Img_pred,Img_true):
+    return 10*torch.log10(torch.max(Img_pred)**2/nn.MSELoss()(Img_pred-Img_true))
+
+def SSIM(Img_pred,Img_true):
+    L = torch.max(Img_true)-torch.min(Img_true)
+    k1 = 0.01
+    k2 = 0.03
+    c1 = (k1*L)**2
+    c2 = (k2*L)**2
+    mu1 = torch.mean(Img_pred)
+    mu2 = torch.mean(Img_true)
+    sig1 = torch.var(Img_pred)
+    sig2 = torch.var(Img_true)
+    sig12 = torch.mean((Img_pred-mu1)*(Img_true-mu2))
+    return (2*mu1*mu2+c1)*(2*sig12+c2)/(mu1**2+mu2**2+c1)/(sig1+sig2+c2)
+    
+    
