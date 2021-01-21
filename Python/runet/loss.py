c61753985e78f48dbd4060c68260225b2c47dd84
qspinat <qspinat@gmail.com>
2021-01-21 11:54:26 +0100

    debug perceptual loss

--- Python/runet/loss.py
+++ Python/runet/loss.py
@@ -11,18 +11,41 @@ import torch
 import torch.nn as nn
 
 
-def perceptual_loss(Img1,Img2,device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
-    model = models.vgg16(pretrained=True).to(device)
-    model.eval()
-    idx = torch.tensor([3,8,15,22,29]) #[1,3,6,8,11,13,15,18,20,22,25,27,29]
-    features1 = model.features[:4](Img1)
-    features2 = model.features[:4](Img2)
-    loss = nn.MSELoss()(features1,features2)
-    for i in range(1,idx.shape[0]):
-        features1 = model.features[idx[i-1]:idx[i]+1](features1)
-        features2 = model.features[idx[i-1]:idx[i]+1](features2)
-        loss = loss + nn.MSELoss()(features1,features2)
-    return loss
+class VGGPerceptualLoss(torch.nn.Module):
+    def __init__(self, resize=True,device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
+        super(VGGPerceptualLoss, self).__init__()
+        blocks = []
+        blocks.append(models.vgg16(pretrained=True).features[:4].to(device).eval())
+        blocks.append(models.vgg16(pretrained=True).features[4:9].to(device).eval())
+        blocks.append(models.vgg16(pretrained=True).features[9:16].to(device).eval())
+        blocks.append(models.vgg16(pretrained=True).features[16:23].to(device).eval())
+        for bl in blocks:
+            for p in bl:
+                p.requires_grad = False
+        self.device=device
+        self.blocks = torch.nn.ModuleList(blocks)
+        self.transform = torch.nn.functional.interpolate
+        self.mean = torch.nn.Parameter(torch.tensor([0.485, 0.456, 0.406]).view(1,3,1,1)).to(device)
+        self.std = torch.nn.Parameter(torch.tensor([0.229, 0.224, 0.225]).view(1,3,1,1)).to(device)
+        self.resize = resize
+
+    def forward(self, input, target):
+        if input.shape[1] != 3:
+            input = input.repeat(1, 3, 1, 1).to(self.device)
+            target = target.repeat(1, 3, 1, 1).to(self.device)
+        input = (input-self.mean) / self.std
+        target = (target-self.mean) / self.std
+        if self.resize:
+            input = self.transform(input, mode='bilinear', size=(224, 224), align_corners=False)
+            target = self.transform(target, mode='bilinear', size=(224, 224), align_corners=False)
+        loss = 0.0
+        x = input
+        y = target
+        for block in self.blocks:
+            x = block(x)
+            y = block(y)
+            loss = loss + nn.MSELoss()(x, y)
+        return loss
     
 def PSNR(Img_pred,Img_true):
     return 10*torch.log10(torch.max(Img_pred)**2/nn.MSELoss()(Img_pred,Img_true))
