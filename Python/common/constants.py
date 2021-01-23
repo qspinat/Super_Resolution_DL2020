ccd5f8a594a62a16c5df190d8a0c49df8badfcc6
Thomas Chabal <t.chabal@hotmail.fr>
2021-01-23 14:54:14 +0100

    Changed checkpoint names

--- Python/common/constants.py
+++ Python/common/constants.py
@@ -1,5 +1,5 @@
 DRIVE_ROOT = "/content/drive/MyDrive/"
 DATA_ROOT = DRIVE_ROOT + "road_segmentation_ideal/"
-CHECKPOINTS_FOLDER = '/'.join(DATA_ROOT.split('/')[:-1]) + '/Super_Resolution_DL2020/Python/checkpoints/'
+CHECKPOINTS_FOLDER = '/'.join(DRIVE_ROOT.split('/')[:-1]) + '/Super_Resolution_DL2020/Python/checkpoints/'
 
 DEFAULT_INPUT_SIZE = 256
--- Python/utils/formatter.py
+++ Python/utils/formatter.py
@@ -2,7 +2,12 @@ from datetime import datetime
 from common.constants import CHECKPOINTS_FOLDER
 
 
-def format_checkpoint_name():
+def format_best_checkpoint_name():
     now = datetime.now()
-    checkpoint_file = CHECKPOINTS_FOLDER + f'checkpoints_{now.hour}{now.minute}{now.second}_{now.day}{now.month}{now.year}_best.pth'
+    checkpoint_file = CHECKPOINTS_FOLDER + f'checkpoints_{now.strftime("%d%m%Y")}_{now.strftime("%H%M%S")}_best.pth'
+    return checkpoint_file
+
+def format_current_checkpoint_name():
+    now = datetime.now()
+    checkpoint_file = CHECKPOINTS_FOLDER + f'checkpoints_{now.strftime("%d%m%Y")}_{now.strftime("%H%M%S")}_current.pth'
     return checkpoint_file
