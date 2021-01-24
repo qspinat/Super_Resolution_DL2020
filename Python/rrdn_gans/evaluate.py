6fc0e97a9aea819a4fbbaaf977936d71cca87998
Thomas Chabal <t.chabal@hotmail.fr>
2021-01-24 21:03:47 +0100

    Updated evaluation of esrgan to include all metrics

--- Python/rrdn_gans/evaluate.py
+++ Python/rrdn_gans/evaluate.py
@@ -1,51 +1,61 @@
-from PIL import Image
-import torch
-from torch.utils.data import DataLoader
-from tqdm import tqdm
 import os
+import numpy as np
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
 from ISR.models import RRDN
-from skimage import transform
 
-from common.constants import DRIVE_ROOT, DATA_ROOT, DEFAULT_INPUT_SIZE
+from common.constants import DATA_ROOT, DEFAULT_INPUT_SIZE
 from common.dataset import SatelliteDataset
 from common.transforms import create_transforms
+from common.loss import SSIM, PSNR, VGGPerceptualLoss
 
 
-class RRDNEvaluator:
-    def __init__(self, criterion, weights="gans", patch_size=DEFAULT_INPUT_SIZE):
-        self.weights = weights
-        self.rrdn  = RRDN(weights=weights, patch_size=patch_size)
-        self.criterion = criterion
+class RRDNEvaluation:
+    def __init__(self, weights="gans", patch_size=DEFAULT_INPUT_SIZE, scale_factor=2):
+        train_transforms, test_transforms = create_transforms(patch_size, patch_size, scale_factor=scale_factor, same_size_input_label=True)
+        self.train_dataloader = DataLoader(SatelliteDataset(DATA_ROOT, train_transforms, is_training_set=True), 1)
+        self.test_dataloader = DataLoader(SatelliteDataset(DATA_ROOT, test_transforms, is_training_set=False), 1)
+        
+        self.rrdn  = RRDN(weights=weights)
 
-    def evaluate_set_with_rrdn(self, dataloader, save_folder='train/'):
-        SAVE_PATH = DRIVE_ROOT + "gans_results/"
-        if not os.path.exists(SAVE_PATH + save_folder):
-            os.makedirs(SAVE_PATH + save_folder)
+    def evaluate(self, evaluate_testset=True):
+        dataloader = self.test_dataloader if evaluate_testset else self.train_dataloader
+        MSE = nn.MSELoss()
+        perceptual_loss = VGGPerceptualLoss()
+
+        self.list_PSNR, self.list_SSIM, self.list_MSE, self.list_VGG = [], [], [], []
 
         with torch.no_grad():
             total_loss = 0
-            for idx, sample in tqdm(enumerate(dataloader), total=len(dataloader)):
-                input = sample['image']
+            for sample in tqdm(dataloader):
+                image = sample['image']
                 label = sample["label"]
-                input = input.squeeze(0).permute(1,2,0) * 255
-
-                output = self.rrdn.predict(input)
-                output = transform.resize(output, (output.shape[0] / 2, output.shape[1] / 2))
                 label = label.squeeze().permute(1,2,0)
+                image = image.squeeze().permute(1,2,0) * 255
 
-                loss = self.criterion(torch.Tensor(output), label)
-                total_loss += loss.data.item()
+                output = self.rrdn.predict(image)
 
-            print(f'Total loss for {save_folder[:-1]} dataset with {self.criterion.__repr__()} loss: {total_loss / len(dataloader.dataset)}')
+                output = transform.resize(output, (label.shape[0], label.shape[1]))
+                output = torch.Tensor(output)
 
+                psnr = PSNR(output, label)
+                ssim = SSIM(output, label)
+                mse = MSE(output, label)
 
-    def evaluate_all(self):
-        train_transforms, test_transforms = create_transforms()
-        train_dataset = SatelliteDataset(DATA_ROOT, train_transforms, is_training_set=True)
-        test_dataset = SatelliteDataset(DATA_ROOT, test_transforms, is_training_set=False)
+                output = Variable(output).float().cuda().permute(2,0,1).unsqueeze(0)
+                label = Variable(label).float().cuda().permute(2,0,1).unsqueeze(0)
+                vgg_loss = perceptual_loss(output, label).cpu()
 
-        train_dataloader = DataLoader(train_dataset, 1)
-        test_dataloader = DataLoader(test_dataset, 1)
+                self.list_PSNR.append(psnr)
+                self.list_SSIM.append(ssim)
+                self.list_MSE.append(mse)
+                self.list_VGG.append(vgg_loss)
 
-        self.evaluate_set_with_rrdn(train_dataloader, 'train/')
-        self.evaluate_set_with_rrdn(test_dataloader, 'test/')
+        set_used = "test" if evaluate_testset else "train"
+        print("Mean PSNR of {:.03f} on {} set with std of {:.03f}".format(np.mean(self.list_PSNR), set_used, np.std(self.list_PSNR)))
+        print("Mean SSIM of {:.05f} on {} set with std of {:.05f}".format(np.mean(self.list_SSIM), set_used, np.std(self.list_SSIM)))
+        print("Mean MSE of {:.07f} on {} set with std of {:.07f}".format(np.mean(self.list_MSE), set_used, np.std(self.list_MSE)))
+        print("Mean VGG-Perceptual of {:.05f} on {} set with std of {:.05f}".format(np.mean(self.list_VGG), set_used, np.std(self.list_VGG)))
